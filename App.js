1b0d0cbd72aafdf86fa73253a3e66d01d1f43465
dev4us <bronxofsoul@gmail.com>
2018-07-18 02:55:58 +0900

    UI dev merge
    UI Dev first step

--- App.js
+++ App.js
@@ -1,23 +1,83 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform, ScrollView } from 'react-native';
+
+import ToDo from "./ToDo";
+
+const { width, height } = Dimensions.get("window"); 
 
 export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  }
   render() {
+    const { newToDo } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Watch And Do !</Text>
+        <View style={styles.card}>
+          <TextInput 
+            underlineColorAndroid={'transparent'} 
+            style={styles.input} 
+            placeholder={"New To Do"} 
+            value={newToDo} 
+            onChange={this._controlNewToDo} 
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView>
+            <ToDo />
+          </ScrollView>
+        </View>
       </View>
     );
+  }
+  _controlNewToDo = text => {
+    this.setState({
+      newToDo: text
+    })
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F23657',
+    alignItems: 'center'
   },
+  title:{
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios:{
+        shadowColor:"rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset:{
+          width:0,
+          height:-1
+        }
+      },
+      android:{
+        elevation: 5
+      }
+    })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#DCDCDC",
+    borderBottomWidth: 1,
+    fontSize: 25
+  }
 });
---
+++ ToDo.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+class ToDo extends React.Component{
+    render(){
+        return(
+        <View>
+            <Text>Hello there?</Text>
+        </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({})
+
+export default ToDo;
